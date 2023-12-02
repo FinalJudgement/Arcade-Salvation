57a6417e538cfa28b2ca6aaba79e2c0a4dd3c410
Carlos Silva <silvacj90@gmail.com>
2023-12-02 16:57:45 -0500

    new vid pg, pics pg, new imgs, rmvd gallery word

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const arrowRight = document.querySelector(".dir-right");
 const form = document.querySelector("form");
 const freshMail = document.querySelector(".fresh-mail");
 const newEmail = document.querySelector("#email");
+const vidContainer = document.querySelector("#videoContainer");
+let vidNum = 1;
 
 const load = () => {
   let galleryIndex = 1;
   imgDisplay.src = "assets/ASIMG1.jpg";
-  for (let i = 1; i <= 17; i++) {
+  for (let i = 1; i <= 34; i++) {
     let createImg = document.createElement("img");
     createImg.src = `assets/ASIMG${i}.jpg`;
     createImg.addEventListener("click", (e) => {
@@ -32,22 +34,71 @@ const load = () => {
     imgSlider.children[galleryIndex].style.border = "3px solid transparent";
     imgSlider.children[galleryIndex - 1].style.border = "3px solid yellow";
 
-    imgSlider.scrollTo((galleryIndex - 1) * 85, 0);
+    imgSlider.scrollTo((galleryIndex - 1) * 185, 0);
 
     // console.log(galleryIndex);
   });
   arrowRight.addEventListener("click", () => {
-    if (galleryIndex < 17) {
+    if (galleryIndex < 34) {
       galleryIndex++;
       imgDisplay.src = `assets/ASIMG${galleryIndex}.jpg`;
     }
     imgSlider.children[galleryIndex - 2].style.border = "3px solid transparent";
     imgSlider.children[galleryIndex - 1].style.border = "3px solid yellow";
 
-    imgSlider.scrollTo((galleryIndex - 1) * 85, 0);
+    imgSlider.scrollTo((galleryIndex - 1) * 185, 0);
 
     // console.log(galleryIndex);
   });
+};
+
+const loadFirstVideo = () => {
+  const vid = document.createElement("video");
+  vid.src = `./assets/videos/video-${vidNum}.mp4`;
+  vid.setAttribute("controls", "controls");
+  vidContainer.appendChild(vid);
+};
+
+const loadNextVideo = () => {
+  const exsistingVideo = vidContainer.querySelector("video");
+  let back = document.querySelector("#back-vid");
+  let next = document.querySelector("#next-vid");
+  if (vidNum === 1) {
+    back.style.visibility = "hidden";
+  } else if (vidNum === 11) {
+    next.style.visibility = "hidden";
+  } else {
+    back.style.visibility = "visible";
+    next.style.visibility = "visible";
+  }
+  vidContainer.removeChild(exsistingVideo);
+  const vid = document.createElement("video");
+  vid.src = `./assets/videos/video-${vidNum}.mp4`;
+  vid.setAttribute("controls", "controls");
+  vid.style.visibility = "hidden";
+  vidContainer.appendChild(vid);
+};
+
+const goNext = () => {
+  if (vidNum < 11) {
+    vidNum++;
+  }
+  loadNextVideo();
+  setTimeout(() => {
+    const newVideo = vidContainer.querySelector("video");
+    newVideo.style.visibility = "visible";
+  }, 100);
+};
+
+const goLast = () => {
+  if (vidNum > 1) {
+    vidNum--;
+  }
+  loadNextVideo();
+  setTimeout(() => {
+    const newVideo = vidContainer.querySelector("video");
+    newVideo.style.visibility = "visible";
+  }, 100);
 };
 
 const customEmail = () => {
